74772aee9cf2e5a9d82f963694824008975b6971
Dan Walker <daniel.walker@ncas.ac.uk>
2020-06-19 11:54:36 +0100

    all three separate buttons working-ish

--- forest/barc/toolbar.py
+++ forest/barc/toolbar.py
@@ -12,14 +12,17 @@ class BARC:
         self.figures = figures
         self.document = bokeh.plotting.curdoc()
         self.barcTools = bokeh.models.layouts.Column(name="barcTools")
-        self.barctextstamp =[];
-        self.windbarbs = []
         self.source_polyline = ColumnDataSource(data.EMPTY)
+        self.source_barb = ColumnDataSource(data.EMPTY)
+        self.source_text_stamp = ColumnDataSource(data.EMPTY)
+        self.source_text_stamp.add([],"datasize")
+        self.source_text_stamp.add([],"fontsize")
+        self.source_text_stamp.add([],"colour")
         for figure in self.figures:
             barc_tools = [
                 self.polyLine(),
-                #self.textStamp(figure),
-                #self.windBarb(figure)
+                self.textStamp(),
+                self.windBarb()
                 ]
             #self.figure.tools = barc_tools
             figure.add_tools(*barc_tools)
@@ -49,11 +52,7 @@ class BARC:
         )
         return tool2
 
-    def textStamp(self, figure):
-        self.source_text_stamp = ColumnDataSource(data.EMPTY)
-        self.source_text_stamp.add([],"datasize")
-        self.source_text_stamp.add([],"fontsize")
-        self.source_text_stamp.add([],"colour")
+    def textStamp(self):
         #render_text_stamp = self.figure.circle(x="xs",y="ys",legend_label="X", source=source);
         starting_font_size = 30 #in pixels 
         starting_colour = "orange" #in CSS-type spec 
@@ -66,7 +65,9 @@ class BARC:
         #glyph.text_font_size = '%spx' % starting_font_size
 
         #render_text_stamp = self.figure.add_glyph(self.source_text_stamp, glyph)
-        render_text_stamp = figure.text_stamp(
+        render_lines = []
+        for figure in self.figures:
+            render_lines.append(figure.text_stamp(
                 x="xs", 
                 y="ys", 
                 source=self.source_text_stamp,
@@ -74,9 +75,10 @@ class BARC:
                 text_color="colour",
                 text_font_size="fontsize"
                 )
+                )
                 
         self.source_text_stamp.js_on_change('data', 
-            bokeh.models.CustomJS(args=dict(datasource = render_text_stamp.data_source, starting_font_size=starting_font_size, figure=figure, starting_colour=starting_colour), code="""
+            bokeh.models.CustomJS(args=dict(datasource = self.source_text_stamp, starting_font_size=starting_font_size, figure=self.figures[0], starting_colour=starting_colour), code="""
                 for(var g = 0; g < datasource.data['fontsize'].length; g++)
                 {
                     if(!datasource.data['colour'][g])
@@ -99,7 +101,7 @@ class BARC:
                 """)
         )
         figure.y_range.js_on_change('start',
-            bokeh.models.CustomJS(args=dict(render_text_stamp=render_text_stamp, glyph=render_text_stamp.glyph, figure=figure, starting_font_size=starting_font_size),code="""
+            bokeh.models.CustomJS(args=dict(render_text_stamp=render_lines[0], glyph=render_lines[0].glyph, figure=self.figures[0], starting_font_size=starting_font_size),code="""
 
             for(var g = 0; g < render_text_stamp.data_source.data['fontsize'].length; g++)
             {
@@ -110,26 +112,24 @@ class BARC:
         )
         #render_text_stamp = bokeh.models.renderers.GlyphRenderer(data_source=ColumnDataSource(dict(x=x, y=y, text="X")), glyph=bokeh.models.Text(x="xs", y="ys", text="text", angle=0.3, text_color="fuchsia"))
         tool3 = PointDrawTool(
-                    renderers=[render_text_stamp],
+                    renderers=render_lines,
                     tags= ['barctextstamp'],
                     )
-        if len(self.barctextstamp) == 0:
-            tool3.name = "MainBarcTextStamp"
-        self.barctextstamp.append(tool3)
         return tool3
 
-    def windBarb(self, figure):
-        self.source_barb = ColumnDataSource(data.EMPTY)
-        render_barb = figure.barb(
+    def windBarb(self):
+        render_lines = []
+        for figure in self.figures:
+            render_lines.append( figure.barb(
                 x="xs", 
                 y="ys", 
                 u=-50,
                 v=-50,
                 source=self.source_barb
-                )
+                ))
 
         tool4 = PointDrawTool(
-                    renderers=[render_barb],
+                    renderers=render_lines,
                     custom_icon = 'forest/wind/barb.png',
                     tags= ['barcwindbarb']
                     )
@@ -140,19 +140,32 @@ class BARC:
         for figure in self.figures:
             toolbars.append( figure.toolbar)
             self.barcTools.children.append(
-                ToolbarBox(toolbar=figure.toolbar, toolbar_location="above")
+                ToolbarBox(toolbar=figure.toolbar, toolbar_location="above", visible=True)
             )
         #tools = sum([ toolbar.tools for toolbar in toolbars ], [])
         #tools.append(self.polyLine())
         freehandbutton = bokeh.models.widgets.Button(label="Freehand")
         freehands = list(self.barcTools.select({'tags': ['barcfreehand']}))
-        freehandbutton.js_on_event(ButtonClick, bokeh.models.CustomJS(args=dict(freehands=freehands), code="""
-            console.log("wibble");
+        freehandbutton.js_on_event(ButtonClick, bokeh.models.CustomJS(args=dict(freehands=freehands, toolbars=toolbars), code="""
             var each;
             for(each of freehands) { each.active = true; } 
             """))
 
-        self.barcTools.children.append( freehandbutton)
+        windbarbbutton = bokeh.models.widgets.Button(label="windbarb")
+        windbarbs = list(self.barcTools.select({'tags': ['barcwindbarb']}))
+        windbarbbutton.js_on_event(ButtonClick, bokeh.models.CustomJS(args=dict(windbarbs=windbarbs, toolbars=toolbars), code="""
+            var each;
+            for(each of windbarbs) { each.active = true; } 
+            """))
+
+        textstampbutton = bokeh.models.widgets.Button(label="textstamp")
+        textstamps = list(self.barcTools.select({'tags': ['barctextstamp']}))
+        textstampbutton.js_on_event(ButtonClick, bokeh.models.CustomJS(args=dict(textstamps=textstamps, toolbars=toolbars), code="""
+            var each;
+            for(each of textstamps) { each.active = true; } 
+            """))
+
+        self.barcTools.children.extend( [freehandbutton, windbarbbutton, textstampbutton])
 
 
         return self.barcTools
