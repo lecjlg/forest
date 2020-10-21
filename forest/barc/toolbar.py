57313b39ad230f0fbeaad83c56bce99afd6c83f4
cemachelen <h.l.burns@leeds.ac.uk>
2020-10-21 11:09:19 +0100

    :bulb: :necktie: Tidy up code

--- forest/barc/toolbar.py
+++ forest/barc/toolbar.py
@@ -1,14 +1,40 @@
+"""
+Barc Tool Bar
+---------------------
+
+This module provides a :class:`BARC` to enable the Barc Tool Bar
+
+.. module:: toolbar
+    :synopis: The barc tool bar and meathods required to enable the barc plugin
+    fuctionality
+.. moduleauthors: Dan Walker @ NCAS and Helen Burns & Dan Elis @ CEMAC (Leeds)
+
+.. class:: BARC
+    :members:
+        ToolBar: Creates tool bar of tools listed below
+        polyLine: FreehandDrawTool
+        polyDraw: PolygonDrawToll
+        polyEdit: edit veticies of polygon
+        boxEdit: BoxDraw and edit tool
+        textStamp: method to enable text stamping
+        windBarb: stamp a windbarb
+        weatherFront: Not in use
+        display_glyphs: displays the selected Category of textstamps
+        set_glyphs: set textstamps to display
+        call: Callback function to dynamically alter tool bar
+.. description: This module was developed by NCAS and CEMAC as part of the
+    WCSSP BARC project.
+   Project. This Script does absolutely nothing.
+   :license: BSD-3
+
+"""
 import bokeh.models
-
-import time
-
-from bokeh.models import ColumnDataSource, Paragraph, Select
-from bokeh.models.glyphs import Text, Patch
+from bokeh.models import ColumnDataSource, Select
 from bokeh.core.properties import value
-from bokeh.models.tools import PolyDrawTool, PolyEditTool, BoxEditTool, PointDrawTool, ToolbarBox, FreehandDrawTool, ProxyToolbar, Toolbar
+from bokeh.models.tools import PolyDrawTool, PolyEditTool, BoxEditTool
+from bokeh.models.tools import PointDrawTool, ToolbarBox, FreehandDrawTool
 from bokeh.events import ButtonClick
-from forest import wind, data, tools, redux
-import forest.middlewares as mws
+from forest import wind, data, tools # tools must be loaded
 from . import front
 
 
@@ -16,7 +42,8 @@ class BARC:
     '''
      A class for the BARC features.
 
-     It is attached to to the main FOREST instance in the `main` function of `forest/main.py`.
+     It is attached to to the main FOREST instance in the `main` function of
+     `forest/main.py`.
     '''
     barcTools = None
     source = {}
@@ -34,10 +61,12 @@ class BARC:
         self.starting_colour = "black"  # in CSS-type spec
         self.starting_width = 2
         self.widthPicker = bokeh.models.widgets.Slider(
-            title='Select width', name="barc_width", width=350, end=10.0, start=1.0, value=self.starting_width)
+            title='Select width', name="barc_width", width=350, end=10.0,
+            start=1.0, value=self.starting_width)
         # colour bar picker
         self.colourPicker = bokeh.models.widgets.ColorPicker(
-            title='Select stamp colour:', width=350, name="barc_colours", color=self.starting_colour)
+            title='Select stamp colour:', width=350, name="barc_colours",
+            color=self.starting_colour)
         # Dropdown Menu of stamp categories
         self.stamp_categories = ["group0", "group1", "group2", "group3",
                                  "group4", "group5", "group6", "group7",
@@ -50,25 +79,29 @@ class BARC:
         self.saveArea = bokeh.models.widgets.inputs.TextAreaInput(
             cols=20, max_length=20000)
         self.saveArea.js_on_change('value',
-                                   bokeh.models.CustomJS(args=dict(sources=self.source, saveArea=self.saveArea, figure=self.figures[0]), code="""
-                Object.entries(JSON.parse(saveArea.value)).forEach(([k,v]) => {
-                    sources[k].data = v;
-                    sources[k].change.emit();
-                    if(k.substring(0,10) == 'text_stamp')
-                    {
-                        for(var g = 0; g < sources[k].data['fontsize'].length; g++)
-                        {
-                            sources[k].data['fontsize'][g] = (((sources[k].data['datasize'][g])/ (figure.y_range.end - figure.y_range.start))*figure.inner_height) + 'px';
-                        }
-                    }
-                })
-            """)
+                                   bokeh.models.CustomJS(
+                                   args=dict(sources=self.source,
+                                   saveArea=self.saveArea,
+                                   figure=self.figures[0]), code="""
+        Object.entries(JSON.parse(saveArea.value)).forEach(([k,v]) => {
+            sources[k].data = v;
+            sources[k].change.emit();
+            if(k.substring(0,10) == 'text_stamp')
+            {
+                for(var g = 0; g < sources[k].data['fontsize'].length; g++)
+                {
+                    sources[k].data['fontsize'][g] = (((sources[k].data['datasize'][g])/ (figure.y_range.end - figure.y_range.start))*figure.inner_height) + 'px';
+                }
+            }
+        })
+    """)
                                    )
 
         self.saveButton = bokeh.models.widgets.Button(
             name="barc_save", width=350, label="Save")
         self.saveButton.js_on_click(
-            bokeh.models.CustomJS(args=dict(sources=self.source, saveArea=self.saveArea), code="""
+            bokeh.models.CustomJS(args=dict(sources=self.source,
+                                            saveArea=self.saveArea), code="""
                 var outdict = {}
                 Object.entries(sources).forEach(([k,v]) =>
                 {
@@ -79,17 +112,31 @@ class BARC:
         )
         # from BARC.woff take the index dictionary
         # James's icons correspond pw-000 - pw-099 glyph index 2 to 101
-        glyphIndexMap = {"983040": 2, "983041": 3, "983042": 4, "983043": 5, "983044": 6, "983045": 7, "983046": 8, "983047": 9, "983048": 10,
-                         "983049": 11, "983079": 12, "983080": 13, "983081": 14, "983082": 15, "983083": 16, "983084": 17, "983085": 18, "983086": 19, "983087": 20,
-                         "983088": 21, "983118": 22, "983119": 23, "983120": 24, "983121": 25, "983122": 26, "983123": 27, "983124": 28, "983125": 29, "983126": 30,
-                         "983127": 31, "983157": 32, "983158": 33, "983159": 34, "983160": 35, "983161": 36, "983162": 37, "983163": 38, "983164": 39, "983165": 40,
-                         "983166": 41, "983196": 42, "983197": 43, "983198": 44, "983199": 45, "983200": 46, "983201": 47, "983202": 48, "983203": 49, "983204": 50,
-                         "983205": 51, "983235": 52, "983236": 53, "983237": 54, "983238": 55, "983239": 56, "983240": 57, "983241": 58, "983242": 59, "983243": 60,
-                         "983244": 61, "983274": 62, "983275": 63, "983276": 64, "983277": 65, "983278": 66, "983279": 67, "983280": 68, "983281": 69, "983282": 70,
-                         "983283": 71, "983313": 72, "983314": 73, "983315": 74, "983316": 75, "983317": 76, "983318": 77, "983319": 78, "983320": 79, "983321": 80,
-                         "983322": 81, "983352": 82, "983353": 83, "983354": 84, "983355": 85, "983356": 86, "983357": 87, "983358": 88, "983359": 89, "983360": 90,
-                         "983361": 91, "983391": 92, "983392": 93, "983393": 94, "983394": 95, "983395": 96, "983396": 97, "983397": 98, "983398": 99, "983399": 100,
-                         "983400": 101}
+        glyphIndexMap = {"983040": 2, "983041": 3, "983042": 4, "983043": 5,
+                         "983044": 6, "983045": 7, "983046": 8, "983047": 9,
+                         "983048": 10, "983049": 11, "983079": 12, "983080": 13,
+                         "983081": 14, "983082": 15, "983083": 16, "983084": 17,
+                         "983085": 18, "983086": 19, "983087": 20, "983088": 21,
+                         "983118": 22, "983119": 23, "983120": 24, "983121": 25,
+                         "983122": 26, "983123": 27, "983124": 28, "983125": 29,
+                         "983126": 30, "983127": 31, "983157": 32, "983158": 33,
+                         "983159": 34, "983160": 35, "983161": 36, "983162": 37,
+                         "983163": 38, "983164": 39, "983165": 40, "983166": 41,
+                         "983196": 42, "983197": 43, "983198": 44, "983199": 45,
+                         "983200": 46, "983201": 47, "983202": 48, "983203": 49,
+                         "983204": 50, "983205": 51, "983235": 52, "983236": 53,
+                         "983237": 54, "983238": 55, "983239": 56, "983240": 57,
+                         "983241": 58, "983242": 59, "983243": 60, "983244": 61,
+                         "983274": 62, "983275": 63, "983276": 64, "983277": 65,
+                         "983278": 66, "983279": 67, "983280": 68, "983281": 69,
+                         "983282": 70, "983283": 71, "983313": 72, "983314": 73,
+                         "983315": 74, "983316": 75, "983317": 76, "983318": 77,
+                         "983319": 78, "983320": 79, "983321": 80, "983322": 81,
+                         "983352": 82, "983353": 83, "983354": 84, "983355": 85,
+                         "983356": 86, "983357": 87, "983358": 88, "983359": 89,
+                         "983360": 90, "983361": 91, "983391": 92, "983392": 93,
+                         "983393": 94, "983394": 95, "983395": 96, "983396": 97,
+                         "983397": 98, "983398": 99, "983399": 100, "983400": 101}
         glyphcodes = list(map(int, list(glyphIndexMap.keys())))
         self.allglyphs = glyphcodes
         self.set_glyphs()
@@ -162,15 +209,16 @@ class BARC:
                 alpha=0.3,
                 color="colour", level="overlay")
             )
-        #text = Text(x="xs", y="ys", text=value("abc"), text_color="red", text_font_size="12pt")
-        #render_line1 = figure.add_glyph(self.source['polyline'],text)
+
         tool2 = FreehandDrawTool(
             renderers=[render_lines[0]],
             tags=['barcfreehand'],
             name="barcfreehand"
         )
         self.source['polyline'].js_on_change('data',
-                                             bokeh.models.CustomJS(args=dict(datasource=self.source['polyline'], colourPicker=self.colourPicker, widthPicker=self.widthPicker, saveArea=self.saveArea, sources=self.source), code="""
+            bokeh.models.CustomJS(args=dict(datasource=self.source['polyline'],
+            colourPicker=self.colourPicker, widthPicker=self.widthPicker,
+            saveArea=self.saveArea, sources=self.source), code="""
                 for(var g = 0; g < datasource.data['colour'].length; g++)
                 {
                     if(!datasource.data['colour'][g])
@@ -213,7 +261,9 @@ class BARC:
             name="barcpoly_draw"
         )
         self.source['poly_draw'].js_on_change('data',
-                                              bokeh.models.CustomJS(args=dict(datasource=self.source['poly_draw'], colourPicker=self.colourPicker, widthPicker=self.widthPicker, saveArea=self.saveArea, sources=self.source), code="""
+            bokeh.models.CustomJS(args=dict(datasource=self.source['poly_draw'],
+            colourPicker=self.colourPicker, widthPicker=self.widthPicker,
+            saveArea=self.saveArea, sources=self.source), code="""
                 for(var g = 0; g < datasource.data['colour'].length; g++)
                 {
                     if(!datasource.data['colour'][g])
@@ -279,7 +329,9 @@ class BARC:
             name="barcbox_edit"
         )
         self.source['box_edit'].js_on_change('data',
-                                             bokeh.models.CustomJS(args=dict(datasource=self.source['box_edit'], colourPicker=self.colourPicker, saveArea=self.saveArea, sources=self.source), code="""
+             bokeh.models.CustomJS(args=dict(datasource=self.source['box_edit'],
+             colourPicker=self.colourPicker, saveArea=self.saveArea,
+             sources=self.source), code="""
                 for(var g = 0; g < datasource.data['colour'].length; g++)
                 {
                     if(!datasource.data['colour'][g])
@@ -301,10 +353,8 @@ class BARC:
         returns:
             PointDrawTool with textStamp functionality.
         '''
-        #render_text_stamp = self.figure.circle(x="xs",y="ys",legend_label="X", source=source);
-        starting_font_size = 15  # in pixels
 
-        #render_text_stamp = self.figure.add_glyph(self.source['text_stamp'], glyph)
+        starting_font_size = 15  # in pixels
         render_lines = []
         for figure in self.figures:
             render_lines.append(figure.text_stamp(
@@ -319,7 +369,10 @@ class BARC:
             )
 
         self.source['text_stamp' + glyph].js_on_change('data',
-                                                       bokeh.models.CustomJS(args=dict(datasource=self.source['text_stamp' + glyph], starting_font_size=starting_font_size, figure=self.figures[0], colourPicker=self.colourPicker, widthPicker=self.widthPicker, saveArea=self.saveArea), code="""
+            bokeh.models.CustomJS(args=dict(datasource=self.source['text_stamp' + glyph],
+            starting_font_size=starting_font_size, figure=self.figures[0],
+            colourPicker=self.colourPicker, widthPicker=self.widthPicker,
+            saveArea=self.saveArea), code="""
                 for(var g = 0; g < datasource.data['xs'].length; g++)
                 {
                     if(!datasource.data['colour'][g])
@@ -341,8 +394,9 @@ class BARC:
                 }
                 """)
                                                        )
-        figure.y_range.js_on_change('start',
-                                    bokeh.models.CustomJS(args=dict(render_text_stamp=render_lines[0], figure=self.figures[0]), code="""
+        self.figures[0].y_range.js_on_change('start',
+            bokeh.models.CustomJS(args=dict(render_text_stamp=render_lines[0],
+            figure=self.figures[0]), code="""
             for(var g = 0; g < render_text_stamp.data_source.data['fontsize'].length; g++)
             {
                  render_text_stamp.data_source.data['fontsize'][g] = (((render_text_stamp.data_source.data['datasize'][g])/ (figure.y_range.end - figure.y_range.start))*figure.inner_height) + 'px';
@@ -350,7 +404,6 @@ class BARC:
             render_text_stamp.glyph.change.emit();
             """)
                                     )
-        #render_text_stamp = bokeh.models.renderers.GlyphRenderer(data_source=ColumnDataSource(dict(x=x, y=y, text="X")), glyph=bokeh.models.Text(x="xs", y="ys", text="text", angle=0.3, text_color="fuchsia"))
         tool3 = PointDrawTool(
             renderers=[render_lines[0]],
             tags=['barc' + glyph],
@@ -422,29 +475,23 @@ class BARC:
                 margin=(0, 0, 0, 0)
             )
 
-            button.js_on_event(ButtonClick, bokeh.models.CustomJS(args=dict(buttons=list(self.toolBarBoxes.select({'tags': ['barc' + each]}))), code="""
+            button.js_on_event(ButtonClick, bokeh.models.CustomJS(args=dict(
+            buttons=list(self.toolBarBoxes.select({'tags': ['barc' + each]}))),
+            code="""
                 var each;
                 for(each of buttons) { each.active = true; }
             """))
             buttons.append(button)
         return buttons
 
-#####################################
-#####################################
+# -----------------------------------------------------------------------------
 
     def ToolBar(self):
+        """Barc Tool Bar
+        """
         toolBarList = []
+        # For each figure supplied (if multiple)
         for i, figure in enumerate(self.figures):
-            # label toolbars
-            '''toolBarBoxes.append(
-                Paragraph(
-                text="""Toolbar: Figure %d"""%(i+1),
-                width=200, height=18,
-                css_classes=['barc_p','barc_g%d'%i],
-                )
-            )'''
-        ''' For each figure supplied (if multiple) '''
-        for figure in self.figures:
             barc_tools = []
             figure.add_tools(
                 bokeh.models.tools.UndoTool(tags=['barcundo']),
@@ -461,10 +508,6 @@ class BARC:
                 self.windBarb()
             )
 
-            #q = time.monotonic()
-            #figure.add_tools(*self.weatherFront(figure, i))
-            #print(time.monotonic() - q, "s")
-
             for glyph in self.allglyphs:
                 glyphtool = self.textStamp(chr(glyph))
                 barc_tools.append(glyphtool)
@@ -477,9 +520,6 @@ class BARC:
                     css_classes=['barc_g%d' % i]
                 )
             )
-        #self.barcTools.children.extend( toolBarBoxes )
-        # tools = sum([ toolbar.tools for toolbar in toolbars ], [])self.dropDown.on_change("value", self.call)
-        # tools.append(self.polyLine())
         # standard buttons
         toolBarBoxes = bokeh.models.layouts.Column(children=toolBarList)
         self.toolBarBoxes = toolBarBoxes
@@ -507,7 +547,10 @@ class BARC:
                 aspect_ratio=1,
                 margin=(0, 0, 0, 0)
             )
-            button.js_on_event(ButtonClick, bokeh.models.CustomJS(args=dict(buttons=list(toolBarBoxes.select({'tags': ['barc' + each]}))), code="""
+            button.js_on_event(ButtonClick,
+            bokeh.models.CustomJS(args=dict(
+            buttons=list(toolBarBoxes.select({'tags': ['barc' + each]}))),
+            code="""
                 var each;
                 for(each of buttons) { each.active = true; }
             """))
@@ -529,7 +572,8 @@ class BARC:
                 aspect_ratio=1,
                 margin=(0, 0, 0, 0)
             )
-            button.js_on_event(ButtonClick, bokeh.models.CustomJS(args=dict(buttons=list(toolBarBoxes.select({'tags': ['barc' + each]}))), code="""
+            button.js_on_event(ButtonClick, bokeh.models.CustomJS(
+            args=dict(buttons=list(toolBarBoxes.select({'tags': ['barc' + each]}))), code="""
                 var each;
                 for(each of buttons) { each.active = true; }
             """))
